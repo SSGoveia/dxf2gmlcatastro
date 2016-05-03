cad3136fa42b77cb269138e143a696043a78a228
Patricio Soriano <pasoriano@gmail.com>
2016-05-03 12:16:19 +0200

    Modificando plantillacatastro.py al archivo explicativo de la página de Coordinación Catastro-Registro (http://www.catastro.minhap.es/documentos/formatos_intercambio/CP%20ejemplo%20explicativo.zip)

--- plantillacatastro.py
+++ plantillacatastro.py
@@ -7,7 +7,7 @@ PLANTILLA_1 = """<?xml version="1.0" encoding="utf-8"?>
 <!--Parcela Catastral para entregar a la D.G. del Catastro.-->
 <gml:FeatureCollection xmlns:gml="http://www.opengis.net/gml/3.2" xmlns:gmd="http://www.isotc211.org/2005/gmd" xmlns:ogc="http://www.opengis.net/ogc" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:cp="urn:x-inspire:specification:gmlas:CadastralParcels:3.0" xmlns:base="urn:x-inspire:specification:gmlas:BaseTypes:3.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="urn:x-inspire:specification:gmlas:CadastralParcels:3.0 http://inspire.ec.europa.eu/schemas/cp/3.0/CadastralParcels.xsd" gml:id="ES.LOCAL.1">
    <gml:featureMember>
-      <cp:CadastralParcel gml:id="ES.LOCAL.1A">
+      <cp:CadastralParcel gml:id="ES.LOCAL.CP.1A">
 <!-- Superficie de la parcela en metros cuadrados. Tiene que coincidir con la calculada con las coordenadas.-->
          <cp:areaValue uom="m2">"""
 
@@ -15,9 +15,9 @@ PLANTILLA_2 = """</cp:areaValue>
          <cp:beginLifespanVersion xsi:nil="true" nilReason="other:unpopulated"></cp:beginLifespanVersion>
 <!-- Geometria en formato GML       -->
          <cp:geometry><!-- srs Name codigo del sistema de referencia en el que se dan las coordenadas, que debe coincidir con el de la cartografia catastral -->
-           <gml:MultiSurface gml:id="MultiSurface_ES.LOCAL.1A" srsName="urn:ogc:def:crs:EPSG::{src}">
+           <gml:MultiSurface gml:id="MultiSurface_ES.LOCAL.CP.1A" srsName="urn:ogc:def:crs:EPSG::{src}">
              <gml:surfaceMember>
-               <gml:Surface gml:id="Surface_ES.LOCAL.1A" srsName="urn:ogc:def:crs:EPSG::{src}">
+               <gml:Surface gml:id="Surface_ES.LOCAL.CP.1A" srsName="urn:ogc:def:crs:EPSG::{src}">
                   <gml:patches>
                     <gml:PolygonPatch>
                       <gml:exterior>
@@ -39,7 +39,7 @@ PLANTILLA_3 = """                          </gml:posList>
 <!-- Identificativo local de la parcela. Solo puede tener letras y numeros. Se recomienda (pero no es necesario) poner siempre un digito de control, por ejemplo utilizando el algoritmo del NIF.-->
            <base:Identifier>
              <base:localId>1A</base:localId>
-             <base:namespace>ES.LOCAL</base:namespace>
+             <base:namespace>ES.LOCAL.CP</base:namespace>
            </base:Identifier>
          </cp:inspireId>
          <cp:label/>
