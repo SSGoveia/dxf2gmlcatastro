23f4a860202659e84e6b5275dd0e4773cbeb6554
Patricio Soriano <pasoriano@gmail.com>
2016-05-03 12:23:17 +0200

    Añadiendo ejemplo de archivo py para usarlo en QGIS

---
+++ ejemplo/catastroqgis.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-.
+import dxf2gmlcatastro
+
+# carpeta de trabajo
+path = '/carpeta/archivos/'
+
+# define archivos
+dxf = 'parcelacad.dxf'
+gml = 'catastrogml.gml'
+
+#Define variables
+dxffile = path + dxf
+gmlfile = path + gml
+src = '25830'
+
+# Crea GML
+dxf2gmlcatastro.crea_gml(dxffile, gmlfile, src)
+
+#Añade capa GML a QGIS
+layer = iface.addVectorLayer(gmlfile, "gmlcatastro", "ogr")
